e2a7748d73f331341cd97f5804f2ba39fe2ce834
YoonMin <rladbsals23@gmail.com>
2021-05-17 23:03:39 +0900

    Initial commit

---
+++ src/test/java/study/querydls/QuerydlsApplicationTests.java
@@ -0,0 +1,39 @@
+package study.querydls;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+import study.querydls.entity.Hello;
+import study.querydls.entity.QHello;
+
+import javax.persistence.EntityManager;
+
+import static org.assertj.core.api.Assertions.*;
+
+@SpringBootTest
+@Transactional
+class QuerydlsApplicationTests {
+
+	@Autowired
+	EntityManager em;
+
+	@Test
+	void contextLoads() {
+		Hello hello = new Hello();
+		em.persist(hello);
+
+		JPAQueryFactory query = new JPAQueryFactory(em);
+		QHello qHello = QHello.hello;
+
+		Hello result = query
+				.selectFrom(qHello)
+				.fetchOne();
+
+		assertThat(result).isEqualTo(hello);
+		assertThat(result.getId()).isEqualTo(hello.getId());
+	}
+
+}
