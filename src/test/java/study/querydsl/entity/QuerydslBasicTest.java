89a78d42efc5f05a8320a542c34f7c327f28168c
yoonmin1026 <rladbsals23@gmail.com>
2021-05-23 15:47:39 +0900

    feat : 상수, 문자 더하기
    [상수]
    Expressions.constant("A")
    
    [문자더하기]
    member.username.concat("_").concat(member.age.stringValue())
     - meber.age는 문자타입이 아니기 때문에 stringValue()를 사용하여 cast 해준다.
     - ENUM을 처리할 때도 자주 사용한다.

--- src/test/java/study/querydsl/entity/QuerydslBasicTest.java
+++ src/test/java/study/querydsl/entity/QuerydslBasicTest.java
@@ -3,6 +3,7 @@ package study.querydsl.entity;
 import com.querydsl.core.QueryResults;
 import com.querydsl.core.Tuple;
 import com.querydsl.core.types.dsl.CaseBuilder;
+import com.querydsl.core.types.dsl.Expressions;
 import com.querydsl.jpa.JPAExpressions;
 import com.querydsl.jpa.JPQLQueryFactory;
 import com.querydsl.jpa.impl.JPAQueryFactory;
@@ -411,6 +412,30 @@ public class QuerydslBasicTest {
                         .when(member.age.between(21, 30)).then("21~30살")
                         .otherwise("기타"))
                 .from(member)
+                .fetch();
+
+        for (String s : result) {
+            System.out.println("s = " + s);
+        }
+    }
+
+    @Test
+    void constant() {
+        List<Tuple> result = queryFactory
+                .select(member.username, Expressions.constant("A"))
+                .from(member)
+                .fetch();
+        for (Tuple tuple : result) {
+            System.out.println("tuple = " + tuple);
+        }
+    }
+
+    @Test
+    void concat() {
+        List<String> result = queryFactory
+                .select(member.username.concat("_").concat(member.age.stringValue()))
+                .from(member)
+                .where(member.username.eq("member1"))
                 .fetch();
 
         for (String s : result) {
