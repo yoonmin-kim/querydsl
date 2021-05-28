6caa7c9fcd44a9a21300024181f32edeadaa3d7f
yoonmin1026 <rladbsals23@gmail.com>
2021-05-29 01:04:52 +0900

    feat : 사용자 정의 리포지토리
    엔티티에 한해서 공통적이면 스프링dataJPA의 특성을 살려서 인터페이스로
    구현한다.
    
    커스텀 기능이면 별도의 인터페이스를 짜고 JpaRepository와 함께
    상속받는다. 커스텀기능에 대한 인터페이스 구현체는 기존의 JpaRepository를
    상속받은 인터페이스명+Impl로 클래스명을 작성한다.(규칙)

--- src/test/java/study/querydsl/repository/MemberRepositoryTest.java
+++ src/test/java/study/querydsl/repository/MemberRepositoryTest.java
@@ -4,8 +4,12 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
+import study.querydsl.dto.MemberSearchCondition;
+import study.querydsl.dto.MemberTeamDTO;
 import study.querydsl.entity.Member;
+import study.querydsl.entity.Team;
 
+import javax.persistence.EntityManager;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -14,6 +18,8 @@ import static org.assertj.core.api.Assertions.assertThat;
 @Transactional
 class MemberRepositoryTest {
 
+    @Autowired
+    EntityManager em;
     @Autowired
     MemberRepository memberRepository;
 
@@ -30,5 +36,31 @@ class MemberRepositoryTest {
 
         List<Member> result2 = memberRepository.findByUsername("member1");
         assertThat(result2).containsExactly(member);
+    }
+
+    @Test
+    void searchTest() {
+        Team teamA = new Team("teamA");
+        Team teamB = new Team("teamB");
+        em.persist(teamA);
+        em.persist(teamB);
+
+        Member member1 = new Member("member1", 10, teamA);
+        Member member2 = new Member("member2", 20, teamA);
+
+        Member member3 = new Member("member3", 30, teamB);
+        Member member4 = new Member("member4", 40, teamB);
+        em.persist(member1);
+        em.persist(member2);
+        em.persist(member3);
+        em.persist(member4);
+
+        MemberSearchCondition condition = new MemberSearchCondition();
+        condition.setAgeGoe(35);
+        condition.setAgeLoe(40);
+        condition.setTeamName("teamB");
+
+        List<MemberTeamDTO> result = memberRepository.search(condition);
+        assertThat(result).extracting("username").containsExactly("member4");
     }
 }
